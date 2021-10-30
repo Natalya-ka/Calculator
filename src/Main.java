7d83a0a3ffb6250ccdc6969291dae621b567089f
Natalya-ka <93324362+Natalya-ka@users.noreply.github.com>
2021-10-30 22:58:40 +0200

    All done Calc 2

--- src/Main.java
+++ src/Main.java
@@ -8,13 +8,13 @@ class Main {
 
         Scanner input = new Scanner(System.in);
 
-        System.out.println("Choose an operator: +, -, *, or /");
+        System.out.println("Выберите оператор: +, -, *, или /");
         operator = input.next().charAt(0);
 
-        System.out.println("Enter first number");
+        System.out.println("ВВедите первое значение");
         number1 = input.nextDouble();
 
-        System.out.println("Enter second number");
+        System.out.println("ВВедите второе значение");
         number2 = input.nextDouble();
 
         switch (operator) {
@@ -36,14 +36,14 @@ class Main {
 
             case '/':
                 if (number2==0){
-                    System.out.println("Error!Can't be divided by zero.");
+                    System.out.println("Ошибка!Делить на ноль нельзя.");
                 }
                 result = number1 / number2;
                 System.out.println(number1 + " / " + number2 + " = " + result);
                 break;
 
             default:
-                System.out.println("Invalid operator!");
+                System.out.println("Не коррентный оператор!");
                 break;
         }
     }
